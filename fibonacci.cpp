e6762f2e3482ef2337986bc1731c235daf36a3c7
Link <h26281660@gmail.com>
2023-02-26 12:01:52 +0800

    fibonacci

---
+++ fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci (int index);
+
+int main () {
+  int index;
+
+  cout << "Enter fibonacci index number: " << endl;
+  cin >> index;
+  
+  cout << "Answer: " << fibonacci(index) << endl; 
+} 
+
+int fibonacci (int index) {
+  if (index <= 1) {
+    return 1;
+  }
+
+  return fibonacci(index - 1) + fibonacci(index - 2);
+}
+
