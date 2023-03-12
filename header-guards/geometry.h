0366a7802c7915208b432c5d451637ac0d02d316
Link <h26281660@gmail.com>
2023-03-12 08:16:06 +0800

    header guard

---
+++ header-guards/geometry.h
@@ -0,0 +1,7 @@
+// #ifndef GEOMETRY_H
+// #define GEOMETRY_H
+#pragma once
+
+#include "square.h"
+
+// #endif
---
+++ header-guards/square.h
@@ -0,0 +1,10 @@
+// #ifndef SQUARE_H
+// #define SQUARE_H
+#pragma once
+
+int getSquareSides()
+{
+  return 4;
+}
+
+// #endif
